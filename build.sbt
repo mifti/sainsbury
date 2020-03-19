d016befe0efa5476498a867feedf197412e52c19
Muhammad Ifitkhar <iftikhar4jan@yahoo.com>
2020-03-19 14:40:21 +0500

    added json parser dependency and create json parser wrapper object, udpated gitignore file

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
 libraryDependencies += "net.ruippeixotog" %% "scala-scraper" % "2.2.0"
+libraryDependencies += "org.json4s" %% "json4s-ast" % "3.6.7"
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.6.7"
---
+++ src/main/scala/com/mi/sainsbury/jsonparser/Json4s.scala
@@ -0,0 +1,18 @@
+package com.mi.sainsbury.jsonparser
+
+import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
+
+/**
+ * object of Json4s
+ */
+object Json4s {
+  
+  /**
+   * method to beautify json object as string
+   */
+  def beautifyJson(json: JValue) : String = {
+    return pretty(render(json))
+  }
+}
